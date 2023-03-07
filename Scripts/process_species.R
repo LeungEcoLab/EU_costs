0bc6c8d091477fa7f010f2209168e8042e193508
Morgane Henry <61826134+morgane-henry@users.noreply.github.com>
2023-03-07 15:58:08 -0500

    Update process_species.R

--- Scripts/process_species.R
+++ Scripts/process_species.R
@@ -4,7 +4,7 @@ library(dplyr)
 
 ##This script build the dataframe for estimating unknown costs of IAS in EU countries
 #Need to have the combination of 
-  #species/sector/type of cost
+#species/sector/type of cost
 
 # Determine what combination of species/type of cost etc ------------------
 
@@ -12,7 +12,7 @@ library(dplyr)
 eu.df.full <- read.csv("/Users/morganehenry/Documents/GitHub/EU_extrapolation/data/EU_extrapol_processed_data.csv")
 
 #Only include data at the country level
-  
+
 eu.df.country <- eu.df.full[eu.df.full$Spatial_scale == "Country",]
 
 # Country only data + combination of species, sector and type of cost ----------------------------------------
@@ -25,20 +25,11 @@ comb.unique4 <- data.frame(comb.unique= unique(comb4)) #need as dataframe to use
 ##Convert this vector to a dataframe
 df.comb4 <- separate(comb.unique4, col= "comb.unique", c("Species", "Impacted_sector", "Type_of_cost_merged"), sep=" , ")
 
-
 # Prepare data for extrapolation from species.csv -------------------------
 
 ##Open dataframe containing the presence of invasive species in Europe
 
-sp <- read.csv("/Users/morganehenry/Documents/GitHub/EU_extrapolation/data/species.csv", header=T)
-
-##Fill blank with NA
-sp[sp == ""] <- NA 
-
-##Separate column "taxon" on genus, species, subspecies
-df.sp <- separate(sp, col= "Taxon", c("genus", "Species", "other","other1","other2","other3","other4","other5","other6","other7"), sep=" ")
-sp$genus <- df.sp$genus
-sp$Species <- paste(df.sp$genus, df.sp$Species)
+sp <- read.csv("/Users/morganehenry/Documents/GitHub/EU_extrapolation/data/species_EU_countries.csv", header=T)
 
 # How many specie are present in both the InvaCost data and the extrapolation df --------
 nb.sp4 <- intersect(df.comb4$Species, sp$Species) ; print(length(unique(nb.sp4))) #Country only data, combination of species, impacted sector and type of cost: 46
@@ -47,16 +38,6 @@ nb.sp4 <- intersect(df.comb4$Species, sp$Species) ; print(length(unique(nb.sp4))
 country.df <- read.csv("/Users/morganehenry/Documents/GitHub/EU_extrapolation/data/Country_new.csv")
 country.df <- country.df[,-1]
 country.df <- country.df %>% rename(Official_country = Country)
-
-##Create new column names Official_country to match InvaCost column
-sp$Official_country <- sp$Location
-
-##Change Czech Republic to Czechia in sp
-sp$Official_country[sp$Location == "Czech Republic"] <- "Czechia"
-
-territories <- c("Azores", "Balearic Islands", "Canary Islands", "Faroe Islands", "Gibraltar", "Guernsey", "Isle of Man", "Jersey", "Madeira", "Svalbard and Jan Mayen")
-terr1 <- which(sp$Location %in% territories)
-sp <- sp[-terr1,]
 
 ##Merge sp dataframe with the country information (GDP, population, etc)
 sp <- merge(sp, country.df, by="Official_country")
